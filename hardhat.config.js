9fc5f5e53ca7714ae6f5ace64824ec421796bd3c
Vladimir Stemkovski <stemkovs@yahoo.com>
2021-12-18 12:44:27 -0500

    genericFarmV2

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,6 @@ require("solidity-coverage")
 require("@nomiclabs/hardhat-ganache")
 require("hardhat-gas-reporter");
 
-
 const { task } = require("hardhat/config")
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -56,52 +55,33 @@ module.exports = {
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     excludeContracts: ["contracts/mocks/", "contracts/lib/", "contracts/uniswapv2/"],
   },
-  hardhat: {
-    forking: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-    },
-  },
   namedAccounts: {
     deployer: {
       default: 0,
     },
     dev: {
-      // Default to 1
       default: 1,
-      // dev address mainnet
-      // 1: "",
     },
   },
   networks: {
     hardhat: {
-      chainId: 31337,
-      accounts,
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts,
-      chainId: 3,
-      live: true,
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 13011904
+      },
+      live: false,
       saveDeployments: true,
+      allowUnlimitedContractSize: false,
+      tags: ["test", "local"]
     },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${process.env.KOVAN_PRIVATE_KEY}`],
-      chainId: 42,
-      live: true,
-      saveDeployments: true,
-      tags: ["staging"],
-      gasPrice: 20000000000,
-      gasMultiplier: 2,
+    goerli: {
+      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_ID,
+      accounts
     },
     mainnet: {
-        url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-        accounts: [`0x${process.env.MAINNET_PK}`],
-        chainId: 1,
-        live: true,
-        saveDeployments: true,
-        gasPrice: 165 * 1000000000,
-      },
+      url: 'https://mainnet.infura.io/v3/' + process.env.INFURA_ID,
+      accounts
+    }
   },
   preprocess: {
     eachLine: removeConsoleLog((bre) => bre.network.name !== "hardhat" && bre.network.name !== "localhost"),
